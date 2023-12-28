f846afcde1b8bcd7e03af94db14578ca54d4cadc
Marcelo Canaparro <Marcelo.Canaparro@ctw.bmwgroup.com>
2023-12-28 19:46:59 +0000

    Adds templates

--- trydjango/views.py
+++ trydjango/views.py
@@ -3,7 +3,8 @@ To render html web pages
 """
 
 from django.http import HttpResponse
-
+from articles.models import Article
+from django.template.loader import render_to_string
 
 
 def home_view(request):
@@ -11,12 +12,12 @@ def home_view(request):
     Take in a request
     Return HTML as response
     """
-    
-    HTML_Title = f"""
-    <h1>{title}</h1>
-    """
+    article_obj = Article.objects.get(id=2)
+    context = {
+        "id" : article_obj.id,
+        "title" : article_obj.title,
+        "content" : article_obj.content
+    }
 
-    HTML_content = f"""
-    <h1>Hello {content}</h1>
-    """
+    HTML_STRING = render_to_string("home-view.html", context=context)
     return HttpResponse(HTML_STRING)
