847f354b0e3f7049babbdd3166d1ea37c31ff413
Marcelo Canaparro <Marcelo.Canaparro@ctw.bmwgroup.com>
2023-12-29 20:17:21 +0000

    Adds login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+def login_view(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            context = {"error": "Invalid username or password"}
+        login(request, user)
+        return redirect('/')
+    return render(request, "accounts/login.html", context=context)
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/login/')
+    return render(request, "accounts/logout.html", context={})
+
+def register_view(request):
+
+    return render(request, "accounts/login.html", context={})
