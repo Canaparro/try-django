0bd0c73d411837afe3c0c7149bae9aafebaaedb2
Marcelo Canaparro <Marcelo.Canaparro@ctw.bmwgroup.com>
2023-12-29 18:08:54 +0000

    Adds article view

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Article
+
 # Create your views here.
+
+def article_detail_view(request, id: int = None):
+    article = Article.objects.get(id=id) if id else None
+    
+    context = {
+        "article": article
+    }
+
+    return render(request, "articles/detail.html", context=context)
--- trydjango/views.py
+++ trydjango/views.py
@@ -6,14 +6,15 @@ from django.http import HttpResponse
 from articles.models import Article
 from django.template.loader import render_to_string
 
-
 def home_view(request):
     """
     Take in a request
     Return HTML as response
     """
     article_obj = Article.objects.get(id=2)
+    article_objs = Article.objects.all()
     context = {
+        "article_objs": article_objs,
         "id" : article_obj.id,
         "title" : article_obj.title,
         "content" : article_obj.content
