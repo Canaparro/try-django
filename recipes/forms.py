3df2e354663f1954d84679ba539ced4ed4996f91
Marcelo Canaparro <Marcelo.Canaparro@ctw.bmwgroup.com>
2024-01-09 16:51:07 +0000

    Adds css and form configs to recipes

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,10 +1,30 @@
 from django import forms
 
-from .models import Recipe
+from .models import Recipe, RecipeIngredient
 
 
 class RecipeForm(forms.ModelForm):
+    required_css_class = 'required-field'
+    name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Recipe name'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}))
 
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'directions']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        config = {
+            'class': 'form-control'
+        }
+        for field in self.fields:
+            config['placeholder'] = f'Recipe {field}'
+            self.fields[field].widget.attrs.update(config)
+
+
+class RecipeIngredientForm(forms.ModelForm):
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ['name', 'quantity', 'unit']
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.forms.models import modelformset_factory
 from django.contrib.auth.decorators import login_required
 
-from recipes.forms import RecipeForm
-from .models import Recipe
+from recipes.forms import RecipeForm, RecipeIngredientForm
+from .models import Recipe, RecipeIngredient
 
 # Create your views here.
 
@@ -43,11 +44,22 @@ def recipe_create_view(request):
 def recipe_update_view(request, id=None):
     obj = get_object_or_404(Recipe, id=id, user=request.user)
     form = RecipeForm(request.POST or None, instance=obj)
+    recipe_ingredient_form_set_factory = modelformset_factory(
+        RecipeIngredient, form=RecipeIngredientForm, extra=0)
+    qs = obj.recipeingredient_set.all()
+    form_set = recipe_ingredient_form_set_factory(
+        request.POST or None, queryset=qs)
     context = {
         "form": form,
+        "form_set": form_set,
         "object": obj
     }
-    if form.is_valid():
-        obj.save()
+    if form.is_valid() and form_set.is_valid():
+        parent = form.save()
+        for ingredient_form in form_set:
+            child = ingredient_form.save(commit=False)
+            if child.recipe is None:
+                child.recipe = parent
+            child.save()
         context['message'] = 'Data saved.'
     return render(request, "recipes/create-update.html", context=context)
